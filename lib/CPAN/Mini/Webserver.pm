b4b290780d3565dfa229c618fb5facc8271aee40
Leon Brocard <acme@astray.com>
2008-08-28 14:39:17 +0000

    r874@tigger:  acme | 2008-08-28 15:39:16 +0100  release 0.41

--- lib/CPAN/Mini/Webserver.pm
+++ lib/CPAN/Mini/Webserver.pm
@@ -39,7 +39,7 @@ has 'distvname'           => ( is => 'rw' );
 has 'filename'            => ( is => 'rw' );
 has 'index' => ( is => 'rw', isa => 'CPAN::Mini::Webserver::Index' );
 
-our $VERSION = '0.40';
+our $VERSION = '0.41';
 
 sub service_name {
     "$ENV{USER}'s minicpan_webserver";
