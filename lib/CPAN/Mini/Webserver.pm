974f31cc3282e0c0b9fb3c17b0f1dffa23651253
Leon Brocard <acme@astray.com>
2008-09-18 16:11:10 +0000

    r884@tigger:  acme | 2008-09-18 17:11:07 +0100  add a human-readable license

--- lib/CPAN/Mini/Webserver.pm
+++ lib/CPAN/Mini/Webserver.pm
@@ -662,5 +662,7 @@ Leon Brocard <acme@astray.com>
 
 Copyright (C) 2008, Leon Brocard.
 
+=head1 LICENSE
+
 This module is free software; you can redistribute it or 
 modify it under the same terms as Perl itself.
